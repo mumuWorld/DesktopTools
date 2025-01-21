61b45373fd7750877baabc9c68e265ed80f4600c
mumu <617958070@qq.com>
2025-01-21 18:16:49 +0800

    add: 添加

--- renderer.js
+++ renderer.js
@@ -1,7 +1,6 @@
 const information = document.getElementById('info')
 information.innerText = `本应用正在使用 Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), 和 Electron (v${versions.electron()})`
 
-console.log('test->lgoggdgs2') // 打印 'pong'
 
 const func = async () => {
     console.log('test->lgoggdgs') // 打印 'pong'
